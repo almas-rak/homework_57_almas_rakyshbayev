3fe2ab28c81e83e1050acfc8fa393762f69b6b3f
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-03 10:40:45 +0600

    регистрация моделей в админке

--- source/issue_tracker/admin.py
+++ source/issue_tracker/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
+from issue_tracker.models import Task
+from issue_tracker.models import Status
+from issue_tracker.models import Type
+
+
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'summary', 'description', 'status', 'type', 'created_at', 'updated_at')
+    list_editable = ('summary', 'description', 'status', 'type',)
+
+
+admin.site.register(Task, TaskAdmin)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_editable = ['name']
+
+
+admin.site.register(Status, StatusAdmin)
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_editable = ['name']
+
+
+admin.site.register(Type, TypeAdmin)
