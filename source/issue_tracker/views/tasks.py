c240f23716dd2aec6fcfdfbf11235f21b5633c83
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-03 13:49:02 +0600

    добавлен метод создания задачи

--- source/issue_tracker/views/tasks.py
+++ source/issue_tracker/views/tasks.py
@@ -1,5 +1,8 @@
-from django.views.generic import TemplateView
+from django.core.handlers.wsgi import WSGIRequest
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, RedirectView
 
+from issue_tracker.forms.form_task import TaskForm
 from issue_tracker.models import Task
 
 
@@ -10,3 +13,19 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = Task.objects.filter(is_deleted=False)
         return context
+
+
+class CreateTask(TemplateView):
+    template_name = 'add_task.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = TaskForm()
+        return context
+
+    def post(self, request: WSGIRequest, *args, **kwargs):
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task = form.save()
+            return redirect('index')
+        return render(request, 'add_task.html', context={'form': form})
