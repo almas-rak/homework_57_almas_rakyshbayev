97462f45108efe268dddffbeb772f3ea0c35dab4
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-03 14:03:08 +0600

    в ссылки добавлена сслыка на добавления

--- source/issue_tracker/urls.py
+++ source/issue_tracker/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from issue_tracker.views.tasks import IndexView
+from issue_tracker.views.tasks import IndexView, CreateTask
 
 urlpatterns = [
     path("", IndexView.as_view(), name='index'),
+    path("add/task/", CreateTask.as_view(), name='create_task'),
 ]
